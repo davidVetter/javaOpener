7a042c47a86de035d31fa72f87123b9b2f56e41c
David Vetter <ddvetter23@gmail.com>
2023-02-03 14:57:40 -0600

    added some string method examples

--- Testing.java
+++ Testing.java
@@ -1,11 +1,31 @@
 class Testing {
     public static void main(String[] args) {
         String name = "David";
+        // Declare an int
         int age = 31;
+        // Declare a string
         String city = "West Fargo";
-
-        String formattedString = String.format("My name is %s. I am from %s. I am %d years old.", name, city, age);
+        // Declare a boolean
+        boolean truth = true;
+        // Formatted string with variables
+        // % + 'letter' for variables - s for string, b for boolean, etc
+        String formattedString = String.format("My name is %s. I am from %s. I am %d years old. Is this true? %b", name,
+                city, age, truth);
         System.out.println("Hello there!");
         System.out.println(formattedString);
+        // returns a strings number of characters *includes spaces if multiple words*
+        System.out.println(formattedString.length());
+        // returns false if string is not 'empty'
+        System.out.println(formattedString.isEmpty());
+        // Make string uppercase * does not change the original string
+        System.out.println(formattedString.toUpperCase());
+
+        // Create new class objects of strings - Each is own place in memory *NOT REFERENCE, truely different even if the string value is the same
+        String test1 = new String("Check");
+        String test2 = new String("Check");
+        // These are different objects in memory and are not equal
+        System.out.println(test1 == test2); // false
+        // How to check if two objects contain the same string
+        System.out.println(test1.equals(test2)); // true
     }
 }
