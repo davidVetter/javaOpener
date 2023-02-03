ab730ad85c75d34e91d7d24e3b3e1f48df00548e
David Vetter <ddvetter23@gmail.com>
2023-02-03 14:41:53 -0600

    added formatted string

--- Testing.java
+++ Testing.java
@@ -1,5 +1,11 @@
 class Testing {
     public static void main(String[] args) {
+        String name = "David";
+        int age = 31;
+        String city = "West Fargo";
+
+        String formattedString = String.format("My name is %s. I am from %s. I am %d years old.", name, city, age);
         System.out.println("Hello there!");
+        System.out.println(formattedString);
     }
 }
