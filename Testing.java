8ea2ef21b5db9ff229d0f7a1ad3c5ecdd8e98e3e
David Vetter <ddvetter23@gmail.com>
2023-02-03 20:17:45 -0600

    added basic calculator

--- Testing.java
+++ Testing.java
@@ -11,6 +11,7 @@ class Testing {
         boolean truth = true;
         // Formatted string with variables
         // % + 'letter' for variables - s for string, b for boolean, etc
+        // USE 'printf(**string**)' to use the format variables directly when outputing to the console
         String formattedString = String.format("My name is %s. I am from %s. I am %d years old. Is this true? %b", name,
                 city, age, truth);
         System.out.println("Hello there!");
@@ -35,12 +36,44 @@ class Testing {
         // Create new 'scanner' - accept user input from the console
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, what is your name? ");
-        // store next input to console in variable
-        String userName = scanner.nextLine();
-        // print users input
-        System.out.println(userName);
-        // close the scanner - clear the memory
+        // System.out.println("Hello, what is your name? ");
+        // // store next input to console in variable
+        // // *Line below can be replaced with int, double, etc to the different data types
+        // // When using a method other than 'nextLine', add an extra nextLine after the nextInt, etc
+        // // deal with the 'enter' in the input buffer from when the system accepted the previous 'int'
+        // String userName = scanner.nextLine();
+        // System.out.println("Give me a number, any number...");
+        // int intNext = Integer.parseInt(scanner.nextLine()); // An alternative to nextInt, etc is using nextLine - works with double, etc
+        // System.out.printf("You gave me: %d %n", intNext);
+        // // print users input
+        // System.out.printf("The name was: %s %n", userName);
+        // // close the scanner - release the memory
+
+        // Calculator code
+        System.out.println("Please enter the first number: ");
+        double calcNum1 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.printf("You entered: %s %n", calcNum1);
+        System.out.println("Please enter the second numebr: ");
+        double calcNum2 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.printf("You entered: %s %n", calcNum2);
+        System.out.println("Please enter the operation you want to perform ('x or *' for multiply, '/' for divide,, '+' for addition and '-' for subtraction)");
+        String operator = scanner.nextLine();
+        System.out.printf("You chose %s %n", operator);
+
+        if (operator.equals("x")||operator.equals("*")) {
+            System.out.printf("%f * %f = %f", calcNum1, calcNum2, calcNum1 * calcNum2);
+        } else if (operator.equals("-")) {
+            System.out.printf("%f - %f = %f", calcNum1, calcNum2, calcNum1 - calcNum2);
+        } else if (operator.equals("+")) {
+            System.out.printf("%f + %f = %f", calcNum1, calcNum2, calcNum1 + calcNum2);
+        } else if (operator.equals("/")) {
+            System.out.printf("%f / %f = %f", calcNum1, calcNum2, calcNum1 / calcNum2);
+        } else {
+            System.out.printf("Invalid operation! Please restart and try again", calcNum1, calcNum2, calcNum1 + calcNum2);
+        }
+        System.out.println();
         scanner.close();
     }
 }
